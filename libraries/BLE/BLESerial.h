06dd967f195798daf52212bf2436599c88429b86
Rochanglien Infimate <chang@qikpod.com>
2018-01-08 11:15:20 +0530

    Lowercased Service and Characterictics to Support Web Bluetooth

--- libraries/BLE/BLESerial.h
+++ libraries/BLE/BLESerial.h
@@ -36,11 +36,11 @@ class BLESerial : public BLEPeripheral, public Stream
     size_t _txCount;
     uint8_t _txBuffer[BLE_ATTRIBUTE_MAX_VALUE_LENGTH];
 
-    BLEService _uartService = BLEService("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+    BLEService _uartService = BLEService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
     BLEDescriptor _uartNameDescriptor = BLEDescriptor("2901", "UART");
-    BLECharacteristic _rxCharacteristic = BLECharacteristic("6E400002-B5A3-F393-E0A9-E50E24DCCA9E", BLEWriteWithoutResponse, BLE_ATTRIBUTE_MAX_VALUE_LENGTH);
+    BLECharacteristic _rxCharacteristic = BLECharacteristic("6e400002-b5a3-f393-e0a9-e50e24dcca9e", BLEWriteWithoutResponse, BLE_ATTRIBUTE_MAX_VALUE_LENGTH);
     BLEDescriptor _rxNameDescriptor = BLEDescriptor("2901", "RX - Receive Data (Write)");
-    BLECharacteristic _txCharacteristic = BLECharacteristic("6E400003-B5A3-F393-E0A9-E50E24DCCA9E", BLENotify, BLE_ATTRIBUTE_MAX_VALUE_LENGTH);
+    BLECharacteristic _txCharacteristic = BLECharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e", BLENotify, BLE_ATTRIBUTE_MAX_VALUE_LENGTH);
     BLEDescriptor _txNameDescriptor = BLEDescriptor("2901", "TX - Transfer Data (Notify)");
 
     void _received(const uint8_t* data, size_t size);
